fe7f0ea1b26dedfa15d3dc4d8c842d4b43d3a401
José Baldino <josemanuelbaldino@gmail.com>
2025-04-07 15:26:38 +0100

    first commit

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,40 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
     
-    // 3. moveToNextQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 4. shuffleQuestions()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
+    
+    moveToNextQuestion() {
+        this.currentQuestionIndex += 1
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1 ; i>0 ; i--) {
+            const j = Math.floor (Math.random() * (i+1));
+            [this.questions [i] , this.questions[j]] = [this.questions[j] , this.questions[i]]
+        }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (answer) {
+            this.correctAnswers += 1;
+        }
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex<this.questions.length) {
+            return false
+        }
+        if (this.currentQuestionIndex=this.questions.length) {
+            return true
+        }
+    }
 }
